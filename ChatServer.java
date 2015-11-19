a42e77d00dae5f9748cafc9f35f1739faca0307a
Michael Wall <michael.wall22@mail.dcu.ie>
2015-11-19 20:22:20 +0000

    Refactored some code
    Changes to names of variables and ordering of classes within the
    document. Tested on localhost and communication works between clients.
    Total clients is updated on the server side correctly. Needs some
    commenting.

--- ChatServer.java
+++ ChatServer.java
@@ -1,92 +1,21 @@
 import java.net.*;
 import java.io.*;
 
-class ServerThread implements Runnable {
-
-    // socket is to be passed in by the creator
-    private Socket socket = null;
-    private int userID;
-    public ClientsArray clients;
-    public MessageBuffer messages;
-    private PrintWriter socketOut;
-
-
-    public ServerThread(MessageBuffer m0, ClientsArray c0, Socket s0, int uID0) {
-
-        this.socket = s0;
-        this.userID = uID0;
-        this.clients = c0;
-        this.messages = m0;
-
-    }
-
-    // Handles the connection
-    public void run() {
-
-        try {
-
-            String inputLine, outputLine, username;
-
-            // Attach a printer to the socket's output stream
-            socketOut =
-                new PrintWriter(socket.getOutputStream(), true);
-
-            // Attach a reader to the socket's input stream
-            BufferedReader in = 
-                new BufferedReader(
-                new InputStreamReader(socket.getInputStream()));
-
-            // Print connection message for user joining server
-            username = in.readLine();
-            messages.addMessage(username + " just joined the chatroom...");
-            System.out.println(username + " just joined the chatroom...");
-
-            // Send data to the client
-            while ((inputLine = in.readLine()) != null) {
-                messages.addMessage(username + " says: " + inputLine);
-                System.out.println(username + " says: " + inputLine);
-            }
-
-            // Close things that were opened
-            socketOut.close();
-            socket.close();
-
-        } catch (SocketException e) {
-
-            System.out.println("Client disconnected");
-            clients.removeServerThread(userID);
-        
-        } catch (IOException e) {
-
-            e.printStackTrace();
-
-        }
-    }
-
-    public int getID() {
-
-        return userID;
-
-    }
-
-    public void sendMessage(String s) {
-        socketOut.println(s);
-    }
-}
-
 public class ChatServer implements Runnable {
+
+    private int totalUsers;
     private ServerSocket serverSocket;
     private ClientsArray clients;
-    // Keeps track of clients so id can be assigned
-    private int totalUsers; 
     private MessageBuffer messages;
     private MessageDistributor distributor;
 
     public ChatServer() {
-        serverSocket = null;
-        clients = new ClientsArray();
-        messages = new MessageBuffer();
-        distributor = new MessageDistributor(clients, messages);
+
+        this.totalUsers = 0;
+        this.serverSocket = null;
+        this.clients = new ClientsArray();
+        this.messages = new MessageBuffer();
+        this.distributor = new MessageDistributor(clients, messages);
         new Thread(this).start();
     }
 
@@ -101,66 +30,103 @@ public class ChatServer implements Runnable {
 
             System.err.println("Could not listen on port: 7777");
             System.exit(-1);
-
         }
 
         while (true) {
             
-            // Always add a new ServerThread
+            // Always try to add a new ServerThread
             try {
+
                 clients.addServerThread(messages, clients, serverSocket.accept());
             } catch (IOException e) {
 
+                e.printStackTrace();
             }
         }
     }
 
     public static void main(String [] args) throws IOException {
-        System.out.println("Hello from the ChatServer");
+
+        // Create a new ChatServer runnable object
+        // this allows the server to be run via command line
         new ChatServer();
     }
 }
 
-class MessageDistributor implements Runnable {
-    private ClientsArray clients;
-    private MessageBuffer messages;
+class MessageBuffer {
 
-    public MessageDistributor(ClientsArray c0, MessageBuffer m0) {
+    private int nextIn, nextOut, amountOccupied, maxMessages;
+    private String[] queue;
 
-        this.clients = c0;
-        this.messages = m0;
-        new Thread(this).start();
+    public MessageBuffer() {
+
+        nextIn = 0; 
+        nextOut = 0;
+        amountOccupied = 0;
+        maxMessages = 10;
+        queue = new String[maxMessages];
     }
 
-    public void run() {
+    public synchronized void addMessage(String s) {
 
-        while (true) {
-            String out = messages.removeMessage();
-            for (int i = 0; i < clients.getSize(); i++) {
-                clients.getServerThread(i).sendMessage(out);
+        try {
+
+            while (amountOccupied == maxMessages) {
+        
+                wait();
             }
+
+            queue[nextIn] = s;
+            nextIn += nextIn % maxMessages;
+            amountOccupied++;
+
+            notifyAll();
+
+        } catch (InterruptedException e) {
+
+            System.out.println("Storing message: " + s + " failed");
         }
     }
 
+    public synchronized String removeMessage() {
+        
+        try {
 
+            while (amountOccupied == 0) {
+                wait();
+            }
+        
+            String out = queue[nextOut];
+            nextOut += nextOut % maxMessages;
+            amountOccupied--;
 
+            notifyAll();
+        
+            return out;
+        } catch (InterruptedException e) {
+
+            System.out.println("Retrieving message: failed");
+            return null;
+        }
+    }
 }
 
 class ClientsArray {
-    private ServerThread[] arr;
+    
     private int numClients, maxClients, totalUsers;
+    private ServerThread[] arr;
 
     public ClientsArray(){
+
+        numClients = 0;
         maxClients = 10;
         totalUsers = 0;
         arr = new ServerThread[maxClients];
-
     }
 
     public synchronized int getSize() {
         
         return numClients;
-
     }
 
     public synchronized void addServerThread(MessageBuffer m, ClientsArray c, Socket s) {
@@ -179,17 +145,21 @@ class ClientsArray {
         numClients++;
         totalUsers++;
         System.out.println("Clients: " + numClients);
-
     }
 
     public synchronized void removeServerThread(int userID) {
 
         for (int i = 0; i < numClients; i++) {
+    
             if (arr[i].getID() == userID) {
+    
                 for (int j = i; j < numClients - 1; j++) {
+    
                     arr[j] = arr[j+1];
                 }
+    
                 numClients--;
+                break;
             }
         }
         System.out.println("Clients: " + numClients);
@@ -198,58 +168,97 @@ class ClientsArray {
     public synchronized ServerThread getServerThread(int i) {
 
         return arr[i];
-
     }
 }
 
-class MessageBuffer {
-    private String[] queue;
-    private int nextIn, nextOut, amountOccupied, maxMessages;
+class MessageDistributor implements Runnable {
 
-    public MessageBuffer() {
-        maxMessages = 10;
-        queue = new String[maxMessages];
-        nextIn = 0; 
-        nextOut = 0;
-        amountOccupied = 0;
+    private ClientsArray clients;
+    private MessageBuffer messages;
+
+    public MessageDistributor(ClientsArray c0, MessageBuffer m0) {
+
+        this.clients = c0;
+        this.messages = m0;
+        new Thread(this).start();
     }
 
-    public synchronized void addMessage(String s) {
+    public void run() {
+
+        while (true) {
+    
+            String out = messages.removeMessage();
+            for (int i = 0; i < clients.getSize(); i++) {
+                clients.getServerThread(i).sendMessage(out);
+            }
+        }
+    }
+}
+
+class ServerThread implements Runnable {
+
+    // socket is to be passed in by the creator
+    private Socket socket = null;
+    private int userID;
+    private ClientsArray clients;
+    private MessageBuffer messages;
+    private PrintWriter socketOut;
+    private String username, inputLine;
+
+
+    public ServerThread(MessageBuffer m0, ClientsArray c0, Socket s0, int uID0) {
+
+        this.socket = s0;
+        this.userID = uID0;
+        this.clients = c0;
+        this.messages = m0;
+    }
+
+    // Handles the connection
+    public void run() {
+
         try {
 
-            while (amountOccupied == maxMessages) {
-                wait();
+            // Attach a printer to the socket's output stream
+            socketOut =
+                new PrintWriter(socket.getOutputStream(), true);
+
+            // Attach a reader to the socket's input stream
+            BufferedReader in = 
+                new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+
+            // Print connection message for user joining server
+            username = in.readLine();
+            messages.addMessage(username + " just joined the chatroom...");
+
+            // Send data to the client
+            while ((inputLine = in.readLine()) != null) {
+
+                messages.addMessage(username + " says: " + inputLine);
             }
 
-            queue[nextIn] = s;
-            nextIn += nextIn % maxMessages;
-            amountOccupied++;
+            // Close things that were opened
+            socketOut.close();
+            socket.close();
 
-            notifyAll();
+        } catch (SocketException e) {
 
-        } catch (InterruptedException e) {
+            messages.addMessage(username + " just left the chatroom...");
+            clients.removeServerThread(userID);
+        } catch (IOException e) {
 
-            System.out.println("Storing message: " + s + " failed");
+            e.printStackTrace();
         }
     }
 
-    public synchronized String removeMessage() {
-        try {
+    public int getID() {
 
-            while (amountOccupied == 0) {
-                wait();
-            }
-            String out = queue[nextOut];
-            nextOut += nextOut % maxMessages;
-            amountOccupied--;
-
-            notifyAll();
-            return out;
-        } catch (InterruptedException e) {
-
-            System.out.println("Retrieving message: failed");
-            return null;
-        }
+        return userID;
     }
 
+    public void sendMessage(String s) {
+
+        socketOut.println(s);
+    }
 }
